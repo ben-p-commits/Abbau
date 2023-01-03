017b52a4dfe4c90f2e469d5bea0ba0830a6e9839
Ben Palmer <benj.c.palmer@gmail.com>
2023-01-03 11:02:35 -0500

    empty proj initial commit

---
+++ Abbau/AbbauApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbbauApp.swift
+//  Abbau
+//
+//  Created by Benjamin Palmer on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct AbbauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
